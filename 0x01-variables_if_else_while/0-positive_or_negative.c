605523132338d1a03ea36622f6a1ce4bb734a1de
thiago1623 <thiago.96t16@gmail.com>
2020-02-06 18:37:12 +0000

    programming_in_c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,19 +1,21 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - funtion that allows proof nombers
+ * Return: always 0 if passed!
+ */
 int main(void)
 {
-    int n;
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    if (n > 0)
-	printf("%d is positive\n", n);
-    else if (n < 0)
-	printf("%d is negative\n", n);
-    else
-	printf("%d is zero\n", n);
-    return (0);
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+	return (0);
 }
