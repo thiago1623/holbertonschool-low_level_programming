d0ba9d92908b0b8073ff3cee8eea65f3fe0e8bd7
thiago1623 <thiago.96t16@gmail.com>
2020-02-06 22:12:43 +0000

    programming_in_c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - prints out a random number
+*
+* Return: Always 0 (success)
+*/
+int man (void)
+{
+	char alphabet = 'a';
+
+	do {
+		putchar ( alphabet);
+		alphabet++;
+	}
+}
