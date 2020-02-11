c197b0f81a0c26c0f838ea08f54c15f1e08532bf
thiago1623 <thiago.96t16@gmail.com>
2020-02-11 18:16:12 +0000

    functions_in_c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ *print_sing - prints the sign of a number.
+ *@n: the variable
+ *Return: return 1, 2, 0 in the exercise.
+ */
+int print_sign(int n)
+{
+	int a = 43, b = 48, c = 45;
+
+	if (n > 0)
+
+	{
+		_putchar(a);
+		return (1);
+	}
+
+	else if (n == 0)
+	{
+		_putchar(b);
+		return (0);
+	}
+
+	else
+	{
+		_putchar(c);
+		return (-1);
+	}
+}
