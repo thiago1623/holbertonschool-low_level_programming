4578635761aa39c1f9ef39b440c9522b76c8fc71
thiago1623 <thiago.96t16@gmail.com>
2020-02-12 03:47:27 +0000

    the_functions_c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * add - Addition of the values
+ *@n: Value to be summed with m
+ *@m: Value that will be summed with n
+ * Return: Always 0.
+ */
+int add(int n, int m)
+{
+	int a;
+
+	a = (n + m);
+	return (a);
+}
