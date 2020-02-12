9d71d264b43b609f121bdc17609c6425f8531b79
thiago1623 <thiago.96t16@gmail.com>
2020-02-12 03:54:06 +0000

    the_functions_c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_to_98  - Print numbers to 98
+ * @n: Range of numbers it will take
+ *
+ * Return: Always 0.
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (n = n; n <= 98; n++)
+		{
+			printf("%d", n);
+			if (!(n == 98))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			else
+				putchar('\n');
+		}
+	}
+	else if (n >= 98)
+	{
+		for (n = n; n >= 98; n--)
+		{
+			printf("%d", n);
+			if (!(n == 98))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			else
+				putchar('\n');
+		}
+	}
+}
