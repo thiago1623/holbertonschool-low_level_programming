049d301246211db44f60a2eeeaed0f513157b876
thiago1623 <thiago.96t16@gmail.com>
2020-02-13 19:05:16 +0000

    function_for_multiplies

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * mul - prints the string "Holberton" from a character array.
+ *@a: this is the variable
+ *@b: this is the variable
+ * Return: c on success.
+ */
+int mul(int a, int b)
+{
+
+	int i = a * b;
+
+	return (i);
+}
