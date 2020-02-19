b0a8e3dd9e59a686087044067e7319dc2e9e27f9
thiago1623 <thiago.96t16@gmail.com>
2020-02-19 00:43:29 +0000

    function that prints a string in rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+B#include "holberton.h"
+/**
+ * print_rev - check the code for Holberton School students.
+ * @s: this is variable
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+	}
+
+	for (i = i - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
