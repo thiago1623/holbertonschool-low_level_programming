b82f58830f3ce1758163fbbfa97a964e915b57f7
thiago1623 <thiago.96t16@gmail.com>
2020-02-20 16:15:53 +0000

    function that concatenates two strings.

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * *_scrat - check the code for Holberton School students.
+ * *_strcat - check the code for Holberton School students.
  * @dest: this is variable 1
  * @src: this is variable 2
  * Return: Always 0.
@@ -15,7 +15,7 @@ char *_strcat(char *dest, char *src)
 	}
 
 	for (count2 = 0; src[count2] != '\0'; count2++)
-      	{
+	{
 		dest[count1 + count2] = src[count2];
 	}
 	return (dest);
