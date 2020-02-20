88c2e4043ac309d887d96dcfe46baa815cdc27e3
thiago1623 <thiago.96t16@gmail.com>
2020-02-20 20:40:50 +0000

    function that compares two strings.

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * _strcmp - check the code for Holberton School students.
+ * @s1: this is the first accountant.
+ * @s2: this is the second accountant.
+ * Return: Always 0.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (*s1 - *s2);
+}
