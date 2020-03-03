bf5380ea90508709f5493d33d440319b466d42ec
thiago1623 <thiago.96t16@gmail.com>
2020-03-03 22:33:35 +0000

    version finish

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,19 +16,13 @@ int **alloc_grid(int width, int height)
 
 	grid = malloc(sizeof(int *) * height);
 	if (grid == NULL)
-	{
-		free(grid);
 		return (NULL);
-	}
 
 	for (col = 0; col < width; col++)
 	{
 		grid[col] = malloc(sizeof(int) * width);
 		if (grid[col] == NULL)
-		{
-			free(grid[col]);
 			return (NULL);
-		}
 	}
 
 	for (row = 0; row < height; row++)
