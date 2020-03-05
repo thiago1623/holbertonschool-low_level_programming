c65e4652e8a2b7ad4930566667afaedb14831f7f
thiago1623 <thiago.96t16@gmail.com>
2020-03-05 21:10:41 +0000

    version finish

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include "holberton.h"
+/**
+ * *_calloc - program that prints its name.
+ * @nmemb: array of character pointers
+ * @size: array of character pointers
+ * Return: p.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	p = malloc(size * nmemb);
+
+	if (!p)
+	{
+		free(p);
+		return (NULL);
+	}
+
+	for (i = 0; i < nmemb; i++)
+		p[i] = 0;
+	return (p);
+}
