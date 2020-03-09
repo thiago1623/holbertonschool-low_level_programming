9a216e9e154e0b116b1292b7da6c8c4aa8421290
thiago1623 <thiago.96t16@gmail.com>
2020-03-09 18:01:52 +0000

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/**
+ * struct dog - check the code for Holberton School Student
+ *@name: the name
+ *@age: the age
+ *@owner: the owner
+ */
+
+struct dog
+{
+
+	char *name;
+
+	float age;
+
+	char *owner;
+};
+#endif
