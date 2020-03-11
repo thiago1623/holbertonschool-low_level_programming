18b7cd4fca4aa314a1efcd76655d33e2a65397cf
thiago1623 <thiago.96t16@gmail.com>
2020-03-11 20:41:18 +0000

    my first test of my exersice

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - program that prints its name.
+ * @name: array of character pointers
+ * @f: array of character pointers
+ * Return: void.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
