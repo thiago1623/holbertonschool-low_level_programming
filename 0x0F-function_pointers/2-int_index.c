3f6bb43f2378dc9ac1c463e2b0f3a0bd307b59d9
thiago1623 <thiago.96t16@gmail.com>
2020-03-11 22:34:10 +0000

    first test of my exersice

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ * int_index - program that prints its name.
+ * @array: array of character pointers
+ * @size: array of character pointers
+ * @cmp: array of character pointers
+ * Return: -1.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	if (array && size && cmp)
+		for(i = 0; i < size; i++)
+			if (cmp(array[i]) != 0)
+				return (i);
+
+	return (-1);
+}
