27e1e8bf5c8a9ecbf22c245d809e9aa059b7bf8a
thiago1623 <thiago.96t16@gmail.com>
2020-03-12 00:44:18 +0000

    first test of my exersice

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - program that prints its name.
+ * @argc: array of character pointers
+ * @argv: array of character pointers
+ * Return: o always.
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+ /*if argc is diferent or 4 my program print Error*/
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+/*cast firs argument to int */
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	if ((argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*' &&
+	     argv[2][0] != '/' && argv[2][0] != '%') || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	if (num2 == 0 && (argv[2][0] == '/' || argv[2][0] == '%'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", (get_op_func(argv[2])(num1, num2)));
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - program that prints its name.
+ * @a: array of character pointers
+ * @b: array of character pointers
+ * Return: a + b.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - program that prints its name.
+ * @a: array of character pointers
+ * @b: array of character pointers
+ * Return: a - b.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - program that prints its name.
+ * @a: array of character pointers
+ * @b: array of character pointers
+ * Return: a * b.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - program that prints its name.
+ * @a: array of character pointers
+ * @b: array of character pointers
+ * Return: a / b.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - program that prints its name.
+ * @a: array of character pointers
+ * @b: array of character pointers
+ * Return: a % b.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
