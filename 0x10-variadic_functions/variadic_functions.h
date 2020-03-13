802b4ef15e08aaea9e3a2da1c96278e0ec8b337b
thiago1623 <thiago.96t16@gmail.com>
2020-03-13 23:29:07 +0000

    firs test

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,13 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+#include <stdlib.h>
+#include <stdio.h>
 #include <stdarg.h>
+
+typedef struct chfmt{
+	char c;
+	void(*f)(va_list);
+}fmt_t;
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
