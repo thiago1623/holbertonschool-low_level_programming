9b26a0ed2991e16f0796adfc25732869df666e1f
thiago1623 <thiago.96t16@gmail.com>
2020-03-13 16:08:09 +0000

    firs test

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * sum_them_all - program that prints its name.
+ * @n: array of character pointers
+ * Return: sum.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list arg;
+	int sum;
+
+	if (n == 0)
+		return (0);
+
+	va_start(arg, n);
+
+	i = 0;
+	sum = 0;
+	while (i < n)
+	{
+		sum += va_arg(arg, int);
+		i++;
+	}
+	va_end(arg);
+	return (sum);
+}
