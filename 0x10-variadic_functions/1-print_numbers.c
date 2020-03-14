e01d0bb3c74fb05340b16481a3521c4d536de002
thiago1623 <thiago.96t16@gmail.com>
2020-03-14 23:59:24 +0000

    final test

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 /**
- * sum_them_all - program that prints its name.
- * @separator: array of character pointers
- * @n: array of character pointers
- * Return: void.
+ * print_numbers - program that prints its name.
+ * @i: array of character pointers
+ * @arg: array of character pointers
+ * Return: arg.
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-        unsigned int i;
-        va_list arg;
+	unsigned int i;
+	va_list arg;
 
 	va_start(arg, n);
 	for (i = 0; i < n; i++)
