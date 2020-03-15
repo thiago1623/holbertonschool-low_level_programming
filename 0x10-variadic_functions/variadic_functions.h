3a3875dc852f0b8c37ec4cdca1a922f645d52a29
thiago1623 <thiago.96t16@gmail.com>
2020-03-15 19:45:26 +0000

    my header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,18 +1,25 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
 
-typedef struct chfmt{
+/**
+ * struc chfmt - struct
+ *@f: the variable.
+ *@c: another the variable
+ *return: dest
+ */
+typedef struct chfmt
+{
 	char c;
-	void(*f)(va_list);
-}fmt_t;
+	void (*f)(va_list);
+} fmt_t;
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
-#endif
+#endif /* VARIADIC_FUNCTIONS_H */
