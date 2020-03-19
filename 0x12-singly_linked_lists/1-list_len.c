41c1ddadd02f6a2be434b873c5d2ccca89f1272e
thiago1623 <thiago.96t16@gmail.com>
2020-03-19 20:14:12 +0000

    first test

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * list_len - finds the number of nodes in a list
+ * @h: singly linked list
+ * Return: number of elements in a linked list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next, i++;
+	}
+	return (i);
+}
