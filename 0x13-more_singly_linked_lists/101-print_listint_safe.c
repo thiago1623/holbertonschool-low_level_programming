fce1fbde92869340a0126d867091088702544fa1
thiago1623 <thiago.96t16@gmail.com>
2020-03-24 20:15:09 +0000

    first test advanced task 1 | function that prints

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * print_listint_safe - define function
+ * @head: head of linked list
+ * Return: what does it return?
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *current = head, *aux = NULL;
+	size_t n_node;/*save the  number of nodes in the list*/
+
+	if (current == NULL)
+		exit(98);
+
+	n_node = 0;
+	while (current)
+	{
+		aux = current;
+		current = current->next;
+		n_node++;
+		printf("[%p] %d\n", (void *)aux, aux->n);
+		if (aux < current)
+		{
+			printf("-> [%p] %d\n", (void *)current, current->n);
+			break;
+		}
+	}
+	return (n_node);
+}
