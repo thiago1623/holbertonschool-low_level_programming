f0c37b0ea86728f1ad13725962b0daa7a306eedb
thiago1623 <thiago.96t16@gmail.com>
2020-03-31 22:03:58 +0000

    first test task 0 | function that reads a text file and prints it to the POSIX standard output.

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - Read a text file and print to POSIX stdout
+ * @filename: char string of files name
+ * @letters: number of letters to read and print
+ * Return: number of letters read and printed, or 0 if error
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t RDcount, WDcount;
+	char *buffer = NULL;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == - 1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+
+	if (buffer == NULL)
+	{
+		free(buffer);
+		return (0);
+	}
+	RDcount = read(fd, buffer, letters);
+	if (RDcount == - 1)
+		return (0);
+	WDcount = write(STDOUT_FILENO, buffer, RDcount);
+	if (WDcount == - 1 || RDcount != WDcount)
+		return (0);
+
+	free(buffer);
+	close(fd);
+	return(WDcount);
+
+}
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -2,6 +2,9 @@
 #define HEADER_FILE
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
