357a829a556b19fa7a724175cb3afcad9a4c948a
thiago1623 <thiago.96t16@gmail.com>
2020-05-02 13:48:11 -0500

    first check task 1 | returns the elements in a linked lists

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - Print all elements of a dlistint_t list
+ * @h: pointer to head of listint_t list
+ *
+ * Return: Number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t elements = 0;
+
+	while (h)
+	{
+		h  = h->next;
+		elements++;
+	}
+	return (elements);
+}
