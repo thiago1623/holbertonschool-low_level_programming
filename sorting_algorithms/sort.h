caa00aa4573e45c9b4209b73596e42882b99364a
thiago <thiago@kali>
2020-06-14 21:53:39 -0500

    [update]-header

--- sorting_algorithms/sort.h
+++ sorting_algorithms/sort.h
@@ -3,7 +3,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -13,9 +12,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void bubble_sort(int *array, size_t size);
