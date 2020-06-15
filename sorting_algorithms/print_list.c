617fff7d4ea56048130f94aefd6dbb39ea4a4e98
thiago <thiago@kali>
2020-06-14 21:53:32 -0500

    [update]-files

--- sorting_algorithms/print_list.c
+++ sorting_algorithms/print_list.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "sort.h"
-
 /**
  * print_list - Prints a list of integers
  *
@@ -8,16 +7,16 @@
  */
 void print_list(const listint_t *list)
 {
-    int i;
+	int i;
 
-    i = 0;
-    while (list)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", list->n);
-        ++i;
-        list = list->next;
-    }
-    printf("\n");
+	i = 0;
+	while (list)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", list->n);
+		++i;
+		list = list->next;
+	}
+	printf("\n");
 }
