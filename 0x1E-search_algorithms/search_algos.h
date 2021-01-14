523fcd0f1c20bd5b8f756c2edcb36b55524160d1
thiago1623 <thiago.96t16@gmail.com>
2021-01-14 16:38:34 -0500

    [UPDATE]--FILES--(add new changes)

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,6 +6,7 @@
 void print_array(int *array, size_t inc, size_t size);
 
 /* main functions */
+int jump_search(int *array, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 #endif
