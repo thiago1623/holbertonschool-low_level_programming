785c4d06112cdff481079c30afd0630c42d42ce1
thiago1623 <thiago.96t16@gmail.com>
2021-01-14 15:51:10 -0500

    [UPDATE]--FILES--(add new changes)

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,9 +2,8 @@
 #define SEARCH_ALGOS
 #include <stdio.h>
 
-/* helpers
+/* helpers*/
 void print_array(int *array, size_t inc, size_t size);
-*/
 
 /* main functions */
 int linear_search(int *array, size_t size, int value);
