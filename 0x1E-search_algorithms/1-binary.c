f0156c0ca7e2c8903a9576585811ffd96e0c10ce
thiago1623 <thiago.96t16@gmail.com>
2021-01-14 16:32:15 -0500

    [UPDATE]--FILES--(add new changes)

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,60 +1,50 @@
 #include "search_algos.h"
-
 /**
- * binary_search - binary search a ascending sorted array to find a value.
- * No duplicates in array. Must print subarray each split.
- * @array: pointer to first element in array to search in
- * @size: number of elements in array
+ * helper - recursive helper function for the binary search
+ * @array: input array
+ * @size: size of the array
+ * @first: first value of the sub array
+ * @last: last value of the sub array
  * @value: value to search for
- * Return: index of matched value in array or -1 if error or not found
+ * Return: Index of value if found, -1 on if not found
+(*
+(* Add a small optimization with:
+(* if(value == array[first]) return (first)
+(* if(value == array[last]) return (last)
+(*
  */
-int binary_search(int *array, size_t size, int value)
+int helper(int *array, size_t size, size_t first, size_t last, int value)
 {
-	int tmp;
-	size_t b = 0;
+	size_t i;
 
-	if (array == NULL || size == 0)
+	printf("Searching in array: %d", array[first]);
+	for (i = first + 1; i <= last; i++)
+		printf(", %d", array[i]);
+	putchar('\n');
+
+	if (first >= last)
 		return (-1);
-	size--;
-	while (b <= size)
-	{
-		print_array(array, b, size);
+	if (value == array[(first + last) / 2])
+		return ((first + last) / 2);
 
-		tmp = (size - b) / 2 + b;
-		if (array[tmp] == value)
-			return (tmp);
-		else if (array[tmp] < value)
-		{
-			if (size % 2 == 1)
-				b = tmp + 1;
-			else
-				b = tmp;
-		}
-		else
-		{
-			if (size % 2 == 1)
-				size = tmp;
-			else
-				size = tmp - 1;
-		}
-	}
+	if (value < array[(first + last) / 2])
+		return (helper(array, size, first, ((first + last) / 2), value));
+	if (value > array[(first + last) / 2])
+		return (helper(array, size, (first + last) / 2 + 1, last, value));
 	return (-1);
 }
 
 /**
- * print_array - print the current array given rules on format
- * @array: array to print
- * @inc: starting incrementer index
- * @size: increment up to this size
- */
-void print_array(int *array, size_t inc, size_t size)
+  * binary_search - binary search algorithm
+  * @array: input array
+  (* must sorted in ascending order
+  * @size: size of the array
+  * @value: value to search for
+  * Return: Index of value if found, -1 on errors or not found
+  */
+int binary_search(int *array, size_t size, int value)
 {
-	printf("Searching in array: ");
-	for (; inc <= size; inc++)
-	{
-		printf("%d", array[inc]);
-		if (inc < size)
-			printf(", ");
-	}
-	printf("\n");
+	if (!array)
+		return (-1);
+	return (helper(array, size, 0, size - 1, value));
 }
